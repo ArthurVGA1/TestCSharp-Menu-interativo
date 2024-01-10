e76d9a026a0b30fbfc764a0ea61481aed990f0a6
Arthur Verissimo <arthurvga05@gmail.com>
2024-01-10 14:50:39 -0300

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSharp_Menu_interativo.Client;
+
+public class Client
+{
+
+    public string? name;
+    public int age;
+    public string? CPF;
+
+    public void SetClient()
+    {
+        Console.WriteLine("Enter customer details:\nCPF:");
+        CPF = Console.ReadLine();
+        Console.WriteLine("Name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+    }
+    public void GetClient()
+    {
+        Console.WriteLine($"CPF: {this.CPF}\nName: {this.name}\nAge: {this.age} years");
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TestCSharp_Menu_interativo.Client;
+
+class Program 
+{
+    static void Main()
+    {
+        Client client = new Client();
+        string? option;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Choose one of the options:\n1 - Register client.\n2 - Show client.\n3 - Close.");
+
+            option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Register client:");
+                    client.SetClient();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Show client:");
+                    client.GetClient();
+                    break;
+
+                case "3":
+                    Console.WriteLine("Close");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
